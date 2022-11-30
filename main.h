56c99167cb594749e214fa88bb3d724722bda6c1
Ontiimeyes <lucassalinroge@gmail.com>
2022-11-30 15:53:00 +0100

    In progress

---
+++ main.h
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 13:15:38 by lsalin            #+#    #+#             */
+/*   Updated: 2022/11/30 15:52:35 by lsalin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+# include "libft/includes/libft.h"
+# include "pipex/includes/pipex.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <sys/wait.h>
+
+typedef struct s_parser{
+	t_command		*command;
+	t_list			**commands;
+	t_parser_state	state;
+	t_tokenizer		*tokenizer;
+	int				token_i;
+} t_parser;
+
+typedef struct s_command{
+	char *command;
+	t_list **args;
+	t_list **out; // pour > 
+	t_list **out_a; // pour >>
+	t_list **in; // pour < 
+	t_list **in_a; // pour <<
+	int	piped; // est-ce que tu passes à la suivante
+} t_command;
+
+typedef struct s_exe
+{
+	int	pid;
+	t_command	*command;
+}	t_exe;
+
+// PIPEX
+
+char	*find_path(char *cmd, char **envp);
+void	first_child(char **argv, char **envp, int pipefd[2], int fd[2]);
+void	second_child(char **argv, char **envp, int pipefd[2], int fd[2]);
+void	error(void);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_print_putstr(void);
+void	free_strs(char *str, char **strs);
+void	close_fd(int pipefd[2], int fd[2]);
+
+// GNL
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE BUFSIZ
+# endif
+
+# if BUFFER_SIZE > 9223372036854775806
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+char	*get_next_line(int fd);
+void	ft_read_line(int fd, char **keep, char **tmp);
+char	*ft_parse_line(char **keep, char **tmp);
+char	*get_before_newline(const char *s);
+char	*get_after_newline(const char *s);
+int		contains_newline(const char *s);
+char	*ft_strdup(const char *s1);
+char	*join_strs(const char *s1, const char *s2);
+void	*ft_malloc_zero(size_t count, size_t size);
+void	ft_free_strs(char **str, char **str2, char **str3);
+
+#endif
