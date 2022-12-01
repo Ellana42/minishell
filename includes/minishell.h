90afb15b5b114867a743d931201feb2a63d5e624
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-12-01 20:55:40 +0100

    Update bonus

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 16:31:12 by lsalin            #+#    #+#             */
-/*   Updated: 2022/11/30 17:06:10 by lsalin           ###   ########.fr       */
+/*   Updated: 2022/12/01 20:52:54 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,21 @@ typedef struct s_exe
 
 // PIPEX
 
-typedef struct s_data
+typedef struct s_structure
 {
     int     argc;
+	int		*pipe;
+	int		input;
+	int		output;
     char    **argv;
     char    **envp;
     char    *cmd;
+	int		nbr_commands;
     int     fd[2];
     int     pipefd[2];
-}   t_data;
+}   t_structure;
+
+// MANDATORY
 
 char	*find_path(t_data *data);
 void	first_child(t_data *data);
@@ -65,6 +71,11 @@ void	error(void);
 void	ft_print_putstr(void);
 void	free_strs(char **str1, char **str2, char **str3);
 void	close_fd(t_data *data);
+
+// BONUS
+void	create_pipes(t_structure *structure);
+void	clean_init_structure(void);
+void	redirection_in_out(t_structure *structure);
 
 // GNL
 
--- srcs/pipex_bonus.c
+++ srcs/pipex_bonus.c
@@ -6,34 +6,46 @@
 /*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:35:10 by lsalin            #+#    #+#             */
-/*   Updated: 2022/12/01 13:48:05 by lsalin           ###   ########.fr       */
+/*   Updated: 2022/12/01 20:55:14 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+void	create_pipes(t_structure *structure)
 {
-	int	pipefd[3][2];
-	int	pid1;
 	int	i;
-
 	i = 0;
-	
-	while (i < 3)
+
+	while (i < structure->nbr_commands - 1) // On boucle tant qu'on n'a pas atteint le nombre d'arguments entrés par l'user
 	{
-		if (pipe(pipefd[i] < 0))
-			return (1);
+		if (pipe(structure->pipe + 2 * i) == -1) // Création d'une paire de fd (pipe) pour chaque fils
+			return (-1); // remplacer par msg d'erreur
 		i++;
 	}
-	
-	pid1 = fork();
+}
 
-	if (pid1 < 0)
-		return (-1);
+void	clean_init_structure(void)
+{
+	t_structure	structure;
 	
-	if (pid1 == 0) // child 1
-	{
-		
-	}
+	structure.argc = -1;
+	structure.input = -1;
+	structure.output = -1;
+	structure.nbr_commands = -1;
+	structure.argc = NULL;
+	structure.envp = NULL;
+	structure.pipe = NULL;
+	structure.cmd = NULL;
+}
+
+// Redirige les fd d'entrée et de sortie transmis vers l'entrée / la sortie standard
+
+void	redirection_in_out(t_structure *structure)
+{
+	if (dup2(structure->input, STDIN_FILENO) == -1)
+		return (-1); // à remplacer par msg d'erreur
+
+	if (dup2(structure->output, STDOUT_FILENO) == -1)
+		return(-1);
 }
