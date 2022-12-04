71313d77c6971ba8554499dddcf6bd3b55afdb9b
Ellana42 <mtmrkaploun@gmail.com>
2022-12-04 18:20:47 +0100

    Catch alloc errors

--- srcs/parsing/tokenizer/push.c
+++ srcs/parsing/tokenizer/push.c
@@ -3,32 +3,44 @@
 int	tokenizer_push_pipe(t_tokenizer *tokenizer)
 {
 	t_token	*token;
+	t_list	*lst;
 
 	token = token_alloc();
 	if (token_init(token, Pipe, NULL))
 		return (1);
-	ft_lstadd_back(tokenizer->tokens, ft_lstnew((void *)token));
+	lst = ft_lstnew((void *)token);
+	if (!lst)
+		return (1);
+	ft_lstadd_back(tokenizer->tokens, lst);
 	return (0);
 }
 
 int	tokenizer_push_string(t_tokenizer *tokenizer)
 {
 	t_token	*token;
+	t_list	*lst;
 
 	token = token_alloc();
 	if (token_init(token, Str, tokenizer->acc->acc))
 		return (1);
-	ft_lstadd_back(tokenizer->tokens, ft_lstnew((void *)token));
+	lst = ft_lstnew((void *)token);
+	if (!lst)
+		return (1);
+	ft_lstadd_back(tokenizer->tokens, lst);
 	return (0);
 }
 
 int	tokenizer_push_eol(t_tokenizer *tokenizer)
 {
 	t_token	*token;
+	t_list	*lst;
 
 	token = token_alloc();
 	if (token_init(token, Eol, NULL))
 		return (1);
-	ft_lstadd_back(tokenizer->tokens, ft_lstnew((void *)token));
+	lst = ft_lstnew((void *)token);
+	if (!lst)
+		return (1);
+	ft_lstadd_back(tokenizer->tokens, lst);
 	return (0);
 }
--- srcs/parsing/tokenizer/push_funnels.c
+++ srcs/parsing/tokenizer/push_funnels.c
@@ -3,43 +3,59 @@
 int	tokenizer_push_out(t_tokenizer *tokenizer)
 {
 	t_token	*token;
+	t_list	*lst;
 
 	token = token_alloc();
 	if (token_init(token, Out, NULL))
 		return (1);
-	ft_lstadd_back(tokenizer->tokens, ft_lstnew((void *)token));
+	lst = ft_lstnew((void *)token);
+	if (!lst)
+		return (1);
+	ft_lstadd_back(tokenizer->tokens, lst);
 	return (0);
 }
 
 int	tokenizer_push_outa(t_tokenizer *tokenizer)
 {
 	t_token	*token;
+	t_list	*lst;
 
 	token = token_alloc();
 	if (token_init(token, Outa, NULL))
 		return (1);
-	ft_lstadd_back(tokenizer->tokens, ft_lstnew((void *)token));
+	lst = ft_lstnew((void *)token);
+	if (!lst)
+		return (1);
+	ft_lstadd_back(tokenizer->tokens, lst);
 	return (0);
 }
 
 int	tokenizer_push_in(t_tokenizer *tokenizer)
 {
 	t_token	*token;
+	t_list	*lst;
 
 	token = token_alloc();
 	if (token_init(token, In, NULL))
 		return (1);
-	ft_lstadd_back(tokenizer->tokens, ft_lstnew((void *)token));
+	lst = ft_lstnew((void *)token);
+	if (!lst)
+		return (1);
+	ft_lstadd_back(tokenizer->tokens, lst);
 	return (0);
 }
 
 int	tokenizer_push_ina(t_tokenizer *tokenizer)
 {
 	t_token	*token;
+	t_list	*lst;
 
 	token = token_alloc();
 	if (token_init(token, Ina, NULL))
 		return (1);
-	ft_lstadd_back(tokenizer->tokens, ft_lstnew((void *)token));
+	lst = ft_lstnew((void *)token);
+	if (!lst)
+		return (1);
+	ft_lstadd_back(tokenizer->tokens, lst);
 	return (0);
 }
