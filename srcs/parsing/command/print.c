a7ce6e33478627b4a66159840af69841047cefa7
Ellana42 <mtmrkaploun@gmail.com>
2022-12-09 14:38:20 +0100

    Small refacto

--- srcs/parsing/command/print.c
+++ srcs/parsing/command/print.c
@@ -5,9 +5,9 @@ void	_print_str(void *s)
 	printf("%s,", (char *)s);
 }
 
-void	_print_str_chain(t_list **lst)
+void	_print_str_chain(t_list *lst)
 {
-	ft_lstiter(*(lst), &_print_str);
+	ft_lstiter(lst, &_print_str);
 }
 
 void	command_print(t_command *command)
@@ -15,15 +15,15 @@ void	command_print(t_command *command)
 	printf("Command :\n");
 	printf("- command : %s\n", command->command);
 	printf("- args : ");
-	_print_str_chain(command->args);
+	_print_str_chain(*command->args);
 	printf("\n- out : ");
-	_print_str_chain(command->out);
+	_print_str_chain(*command->out);
 	printf("\n- outa : ");
-	_print_str_chain(command->out_a);
+	_print_str_chain(*command->out_a);
 	printf("\n- in : ");
-	_print_str_chain(command->in);
+	_print_str_chain(*command->in);
 	printf("\n- ina : ");
-	_print_str_chain(command->in_a);
+	_print_str_chain(*command->in_a);
 	printf("\n");
 	printf("\n");
 }
