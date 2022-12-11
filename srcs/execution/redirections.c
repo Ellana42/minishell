b09a0ac28ccb67d9de89252926fb05a4c8ebd473
Ellana42 <mtmrkaploun@gmail.com>
2022-12-11 12:46:48 +0100

    Fixed memory errors

--- srcs/execution/redirections.c
+++ srcs/execution/redirections.c
@@ -6,7 +6,7 @@
 /*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 17:42:23 by lsalin            #+#    #+#             */
-/*   Updated: 2022/12/10 18:04:02 by lsalin           ###   ########.fr       */
+/*   Updated: 2022/12/11 12:46:34 by mkaploun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ int	get_out_table(t_command *cmd, int **out_table)
 	while (i < outsize)
 	{
 		file_name = lst_get_i(*command_get_out(cmd), i);
-		*out_table[i] = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-		if (*out_table[i] == -1)
+		(*out_table)[i] = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if ((*out_table)[i] == -1)
 			printf("Error !\n"); // TODO gérer erreur
-		last_fd = *out_table[i];
+		last_fd = (*out_table)[i];
 		i++;
 	}
 	return (last_fd);
