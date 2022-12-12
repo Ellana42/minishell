f506c464bf05501920fd02e82e381c6b292cf40f
Ellana42 <mtmrkaploun@gmail.com>
2022-12-12 10:46:23 +0100

    Working echo

--- srcs/builtins/echo/echo.c
+++ srcs/builtins/echo/echo.c
@@ -1,1 +1,59 @@
-#include "echo.c"
+#include "echo.h"
+// TODO return value
+
+int	is_flag(char *arg)
+{
+	if (!arg)
+		return (1);
+	return (ft_strncmp(arg, "-n", 3));
+}
+
+int	looks_like_flag(char *arg)
+{
+	int	i;
+
+	i = 0;
+	if (!arg)
+		return (0);
+	if (ft_strlen(arg) < 2)
+		return (0);
+	if (ft_strncmp(arg, "-n", 2))
+		return (0);
+	i = 1;
+	while (arg[i] == 'n')
+		i++;
+	return (arg[i] == '\0');
+}
+
+int	echo(char **args)
+{
+	int		i;
+	size_t	size;
+	int		n;
+
+	i = 1;
+	n = 0;
+	if (!args)
+		return (1);
+	size = table_get_size(args);
+	if (size <= 1)
+	{
+		printf("\n");
+		return (0);
+	}
+	while (looks_like_flag(args[i]))
+	{
+		n = 1;
+		i++;
+	}
+	while (i < size - 1)
+	{
+		printf("%s ", args[i]);
+		i++;
+	}
+	if (i == size - 1)
+		printf("%s", args[i]);
+	if (!n)
+		printf("\n");
+	return (0);
+}
--- srcs/parsing/utils/table.c
+++ srcs/parsing/utils/table.c
@@ -5,9 +5,23 @@ void	table_print(char **table)
 	int	i;
 
 	i = 0;
+	if (!table)
+		return ;
 	while (table[i])
 	{
 		printf("%d : %s\n", i, table[i]);
 		i++;
 	}
+}
+
+int	table_get_size(char **table)
+{
+	int	i;
+
+	i = 0;
+	if (!table)
+		return (-1);
+	while (table[i])
+		i++;
+	return (i);
 }
