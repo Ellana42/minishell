d88e3c46b662b9726c288c099c7433b0baa3c52b
Ellana42 <mtmrkaploun@gmail.com>
2022-12-13 17:16:48 +0100

    Fixed execve not termining properly

--- srcs/execution/pipex.c
+++ srcs/execution/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 12:45:04 by lsalin            #+#    #+#             */
-/*   Updated: 2022/12/13 15:48:54 by mkaploun         ###   ########.fr       */
+/*   Updated: 2022/12/13 17:15:55 by mkaploun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ int	pipex_launch(t_commands *commands, char **envp)
 		error(msg("Unexpected error.", "", "", 1), &data);
 
 	data = init_struct(commands, envp);
-	/* printf("-----------------------------------------------------\n\n"); */
-	/* print_struct(&data); */
 
 	nbr_commands = commands_get_size(commands);
 	fd[0] = -1;
@@ -46,7 +44,6 @@ int	pipex_launch(t_commands *commands, char **envp)
 		cmd = commands_get_i(data.commands, i);
 
 		fd[0] = get_in_table(cmd, &in_table, &file_name, pipefd);
-		printf("pour %s: fd[0] = %d\n", command_get_name(cmd), fd[0]);
 
 		if (fd[0] == -1)
 			printf("bash: %s: No such file or directory\n", file_name);
@@ -57,17 +54,13 @@ int	pipex_launch(t_commands *commands, char **envp)
 			exit(EXIT_FAILURE);
 		}
 
-		printf("%s created pipe between %d -> %d\n", command_get_name(cmd), pipefd[1], pipefd[0]);
-
 		if (i == nbr_commands - 1)
 			pipefd[1] = STDOUT_FILENO;
 
 		fd[1] = get_out_table(cmd, &out_table, pipefd);
-		printf("pour %s: fd[1] = %d\n", command_get_name(cmd), fd[1]);
 
 		launch_child(data, cmd, fd);
 		waitpid(-1, NULL, 0);
-		printf("cmd %s exited normally \n", command_get_name(cmd));
 		i++;
 	}
 
