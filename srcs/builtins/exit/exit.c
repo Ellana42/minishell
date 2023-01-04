dc86d425ab92c7730cf6520ea9b0d2e7fc0722e8
Ellana42 <mtmrkaploun@gmail.com>
2023-01-04 19:33:07 +0100

    Fixed exit status transmission via builtin exit

--- srcs/builtins/exit/exit.c
+++ srcs/builtins/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: ellana <mtmrkaploun@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 18:02:45 by ellana            #+#    #+#             */
-/*   Updated: 2023/01/04 19:24:51 by mkaploun         ###   ########.fr       */
+/*   Updated: 2023/01/04 19:32:39 by mkaploun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,8 @@ int	builtin_exit(char **args)
 	err_num = 0;
 	if (exit_parse_args(args, &err_num))
 		return (1);
+	if (err_num == 0)
+		err_num = glob_get_exit_status();
 	glob_set_state(0);
 	return ((int )err_num);
 }
