0727a9a7b191b83be8bd8014429d3cb203f30fca
Ellana42 <mtmrkaploun@gmail.com>
2023-01-06 19:11:57 +0100

    Error messages print in one go

--- srcs/error.h
+++ srcs/error.h
@@ -10,9 +10,10 @@
 
 typedef struct sigaction	t_sa;
 
-int	error_msg(char *cmd, char *issue, int err);
-int	error_msg2(char *cmd, char *cmd2, char *issue, int err);
-int error_msg3(char *msg, int err);
-int	set_errnum(int *error, int error_value, int return_value);
+int		error_msg(char *cmd, char *issue, int err);
+int		error_msg2(char *cmd, char *cmd2, char *issue, int err);
+int		error_msg3(char *msg, int err);
+int		set_errnum(int *error, int error_value, int return_value);
+void	write_to_stderr(char *str);
 
 #endif
--- srcs/error_msg.c
+++ srcs/error_msg.c
@@ -1,32 +1,61 @@
 #include "error.h"
 
+void	write_to_stderr(char *str)
+{
+	int	size;
+
+	size = ft_strlen(str);
+	write(2, str, size);
+}
+
 int	error_msg3(char *msg, int err)
 {
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(msg, 2);
-	ft_putstr_fd("\n", 2);
+	char	*tmp;
+	char	*final_message;
+
+	tmp = ft_strjoin("minishell: ", msg);
+	final_message = ft_strjoin(tmp, "\n");
+	free(tmp);
+	write_to_stderr(final_message);
+	free(final_message);
 	return (err);
 }
 
 int	error_msg(char *cmd, char *issue, int err)
 {
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(cmd, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(issue, 2);
-	ft_putstr_fd("\n", 2);
+	char	*tmp;
+	char	*final_message;
+
+	tmp = ft_strjoin("minishell: ", cmd);
+	final_message = ft_strjoin(tmp, ": ");
+	free(tmp);
+	tmp = ft_strjoin(final_message, issue);
+	free(final_message);
+	final_message = ft_strjoin(tmp, "\n");
+	free(tmp);
+	write_to_stderr(final_message);
+	free(final_message);
 	return (err);
 }
 
 int	error_msg2(char *cmd, char *cmd2, char *issue, int err)
 {
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(cmd, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(cmd2, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(issue, 2);
-	ft_putstr_fd("\n", 2);
+	char	*tmp;
+	char	*final_message;
+
+	tmp = ft_strjoin("minishell: ", cmd);
+	final_message = ft_strjoin(tmp, ": ");
+	free(tmp);
+	tmp = ft_strjoin(final_message, cmd2);
+	free(final_message);
+	final_message = ft_strjoin(tmp, ": ");
+	free(tmp);
+	tmp = ft_strjoin(final_message, issue);
+	free(final_message);
+	final_message = ft_strjoin(tmp, "\n");
+	free(tmp);
+	write_to_stderr(final_message);
+	free(final_message);
 	return (err);
 }
 
