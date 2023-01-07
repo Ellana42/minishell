3b1368b0c2952820d9fa6b483a25ab2269bce8a2
Ellana42 <mtmrkaploun@gmail.com>
2023-01-07 11:41:21 +0100

    Fixed err return chained builtin

--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -51,7 +51,7 @@ int	execution_child(t_execution *execution)
 	if (err == 0 && (fd[0] == -1 || fd[1] == -1))
 		err = 1;
 	if (!err)
-		execution_launch_child(execution, err, fd, path);
+		err = execution_launch_child(execution, err, fd, path);
 	close_pipe(fd);
 	minishell_destroy(execution->minishell);
 	parser_destroy(execution->parser);
